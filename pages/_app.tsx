1a84a979caf07d862c79d5aca5bfc84bdd9b5eda
Karin <karin.stenwall@gmail.com>
2023-01-06 21:45:50 +0100

    feat: custom _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,72 @@
+import '../styles/styles.scss';
+import { ReactNode, useEffect } from 'react';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+// import theme from '../styles/theme';
+import { SWRConfig } from 'swr';
+
+const fetcher = (query: string) =>
+  fetch('/api/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  })
+    .then(res => res.json())
+    .then(json => json.data);
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // const getLayout = Component.getLayout || ((page: ReactNode) => page);
+
+  // useEffect(() => {
+  //   // remove the server-side injected CSS
+  //   const jssStyles = document.querySelector('#jss-server-side');
+  //   if (jssStyles) {
+  //     jssStyles?.parentElement?.removeChild(jssStyles);
+  //   }
+  // }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Cook Book</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Nunito:wght@200;300;400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <SWRConfig
+        value={{
+          // refreshInterval: 3000,
+          shouldRetryOnError: true,
+          fetcher
+        }}
+      >
+        <StyledEngineProvider injectFirst>
+          {/* <ThemeProvider theme={theme}> */}
+          <CssBaseline />
+          {/* {getLayout(<Component {...pageProps} />)} */}
+          <Component {...pageProps} />
+          {/* </ThemeProvider> */}
+        </StyledEngineProvider>
+      </SWRConfig>
+    </>
+  );
+};
+
+export default MyApp;
