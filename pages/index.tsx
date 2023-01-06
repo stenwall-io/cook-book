e3b12e0c01dc483bfcf1ece1eb97bb2ebeeb4c2d
Karin <karin.stenwall@gmail.com>
2023-01-06 23:34:54 +0100

    chore: setuo eslint and tsconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,12 @@
-import useSWR from 'swr'
-
-const fetcher = (query: string) =>
-  fetch('/api/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({ query }),
-  })
-    .then((res) => res.json())
-    .then((json) => json.data)
+import useSWR from 'swr';
 
 export default function Index() {
-  const { data, error } = useSWR('{ users { name } }', fetcher)
+  const { data, error } = useSWR('{ users { name } }');
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
 
-  const { users } = data
+  const { users } = data;
 
   return (
     <div>
@@ -25,5 +14,5 @@ export default function Index() {
         <div key={i}>{user.name}</div>
       ))}
     </div>
-  )
+  );
 }
