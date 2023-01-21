ddf235b067ad7e7575e270bcf04eae7001a2b47f
David S <david@stenwall.io>
2023-01-22 00:11:48 +0100

    chore: advanced mongoose connect()

--- db/config/index.ts
+++ db/config/index.ts
@@ -1,5 +1,8 @@
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 import mongoose from 'mongoose';
+
+
+import models from "@models/index"
 
 config();
 
@@ -8,12 +11,43 @@ const options: object = {
   useUnifiedTopology: true
 };
 
-mongoose
-  .connect(process.env.MONGODB_URI, options)
-  .then(() => {
-    console.log('Connected to database.');
-  })
-  .catch(err => {
-    console.log('Cannot connect to database.', err);
-    process.exit();
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    }
+
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => {
+      return mongoose
+    })
+  }
+
+  try {
+    cached.conn = await cached.promise
+  } catch (e) {
+    cached.promise = null
+    throw e
+  }
+
+  initial();
+  console.log('TUTA');
+  return cached.conn
+}
+
+const initial = async () => {
+  await models.Recipe.create({
+    name: 'Testrecept',    
   });
+}
+
+export default dbConnect
