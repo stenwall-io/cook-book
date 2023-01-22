758c1827eb1108c0e85055e2a8d3f1d637e594fe
Karin <karin.stenwall@gmail.com>
2023-01-22 19:46:29 +0100

    fix: upgrade ts to 4.7.4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,7 +37,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;1,300;1,400&display=swap"
