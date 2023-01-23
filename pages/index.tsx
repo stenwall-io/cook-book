96be89fe71ec829b0f42089164e6ddc6353fb373
David S <david@stenwall.io>
2023-01-23 01:32:33 +0100

    fix: use isLoading from useSWR response

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
 import useSWR from 'swr';
 
 export default function Index() {
-  const { data, error } = useSWR('/api/recipes');
+  const { data, error, isLoading } = useSWR('/api/recipes');
   
   if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
-
-  const recipes = data;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div>
       <h1>Cook Book!</h1>
-      {recipes.map((recipe: any, i: number) => (
+      {data.map((recipe: any, i: number) => (
         <div key={i}>{recipe.name}</div>
       ))}
     </div>
