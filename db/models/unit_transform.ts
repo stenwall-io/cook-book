107ad39046fc5aac4291dcec9d3a5de9a67a121f
David S <david@stenwall.io>
2023-01-23 01:31:30 +0100

    fix: pretty code

--- db/models/unit_transform.ts
+++ db/models/unit_transform.ts
@@ -1,5 +1,5 @@
 import { Schema, Model, model, models } from 'mongoose';
-import { IIngredient, IUnit } from '@models/index';
+import { IIngredient, IUnit } from './';
 
 export interface IUnitTransform {
   ingredient_id: IIngredient['_id'];
@@ -8,21 +8,22 @@ export interface IUnitTransform {
   factor: number;
 }
 
-const UnitTransformSchema = new Schema<IUnitTransform>({
-  ingredient_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Ingredient',
-  },
-  unit_a_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Unit',
-  },
-  unit_b_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Unit',
-  },
-  factor: Number,
-});
+const UnitTransformSchema = new Schema<IUnitTransform>(
+  {
+    ingredient_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Ingredient'
+    },
+    unit_a_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Unit'
+    },
+    unit_b_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Unit'
+    },
+    factor: Number
+  }
+);
 
-export default (models.UnitTransform as Model<IUnitTransform>) ||
-  model<IUnitTransform>('UnitTransform', UnitTransformSchema);
+export default (models.UnitTransform as Model<IUnitTransform>) || model<IUnitTransform>('UnitTransform', UnitTransformSchema);
