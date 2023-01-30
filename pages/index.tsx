15b6aae838a6f0c9e7341c92aab6d8d2a8455936
Karin <karin.stenwall@gmail.com>
2023-01-30 15:14:17 +0100

    chore: better import of data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
+import { IRecipe } from '@models/recipe';
 import useSWR from 'swr';
 
 export default function Index() {
-  const { data: recipes, error, isLoading } = useSWR('/api/recipes');
+  const { data, error, isLoading } = useSWR('/api/recipes');
 
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
+
+  const { recipes } = data;
+  console.log(recipes)
 
   return (
     <div>
       <h1>Cook Book!</h1>
       {recipes &&
-        recipes.map((recipe: any, i: number) => (
-          <div key={recipe._id}>{recipe.name}</div>
+        recipes.map((recipe: IRecipe, i: number) => (
+          <div key={i}>{recipe.name}</div>
         ))}
     </div>
   );
